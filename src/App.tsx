34a8f04c2169b4eba667dd194cd2080a6f93515e
miguelszg <ms462974@gmail.com>
2025-06-09 20:10:57 -0600

    Clave

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App() {
   const [error, setError] = useState<string | null>(null);
   const [inputSearch, setInputSearch] = useState<string>('');
 
-  const PEXELS_API_KEY: string = 'TU_CLAVE_PEXELS';
+  const PEXELS_API_KEY: string = 'gDILo5Sow6JafwkIz6inzMpKdont9gV47gyORMzpYkbSRkrkz1oZXVvF';
 
   useEffect(() => {
     fetchPhotos(query);
